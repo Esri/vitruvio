33da1be21d3972af0599f5183e042e3fb586b362
Benjamin Neukom <bneukom@esri.com>
2020-04-28 18:11:15 +0200

    Improved attribute and annotation data structures

--- Vitruvio/Source/Vitruvio/Public/RuleAttributes.h
+++ Vitruvio/Source/Vitruvio/Public/RuleAttributes.h
@@ -1,10 +1,61 @@
 #pragma once
 
 #include "Containers/Array.h"
+#include "Misc/TVariant.h"
 #include "UObject/Object.h"
 #include "RuleAttributes.generated.h"
 
 using FAttributeGroups = TArray<FString>;
+
+UCLASS()
+class VITRUVIO_API UAttributeAnnotation : public UObject
+{
+	GENERATED_BODY()
+	
+};
+
+UCLASS()
+class VITRUVIO_API URangeAnnotation : public UAttributeAnnotation
+{
+	GENERATED_BODY()
+	
+public:
+	float Min;
+	float Max; 
+};
+
+UCLASS()
+class UAnnotationArgument : public UObject
+{
+	GENERATED_BODY()
+
+	TVariant<FString, bool, double> Value;
+public:
+	
+	float GetFloat() const
+	{
+		return Value.Get<double>();
+	}
+	
+	FString GetString() const
+	{
+		return Value.Get<FString>();
+	}
+
+	bool GetBool() const
+	{
+		return Value.Get<bool>();
+	}
+};
+
+UCLASS()
+class VITRUVIO_API UEnumAnnotation : public UAttributeAnnotation
+{
+	GENERATED_BODY()
+
+public:
+	TArray<UAnnotationArgument> Arguments;
+};
 
 UCLASS()
 class VITRUVIO_API URuleAttribute : public UObject
@@ -14,6 +65,8 @@ class VITRUVIO_API URuleAttribute : public UObject
 public:
 	FString Name;
 
+	UPROPERTY()
+	UAttributeAnnotation* Annotation;
 	FAttributeGroups Groups;
 };
 
@@ -33,8 +86,7 @@ class VITRUVIO_API UFloatAttribute : public URuleAttribute
 
 public:
 	float Value;
-	float Min;
-	float Max;
+
 };
 
 UCLASS()
@@ -44,4 +96,13 @@ class VITRUVIO_API UBoolAttribute : public URuleAttribute
 
 public:
 	bool Value;
+};
+
+UCLASS()
+class VITRUVIO_API UColorAttribute : public URuleAttribute
+{
+	GENERATED_BODY()
+
+public:
+    FColor Color;
 };
