6af9e7be4153011abe15d5fd5da067b6cfb61284
Benjamin Neukom <bneukom@esri.com>
2020-07-06 18:18:01 +0200

    Minor cleanup for pre build encoder dll cleanup script (#36)

--- UnrealGeometryEncoder/Source/Scripts/CleanupEncoderDlls.ps1
+++ UnrealGeometryEncoder/Source/Scripts/CleanupEncoderDlls.ps1
@@ -1,15 +1,13 @@
 param ([string]$PluginDir)
-function Remove-Old($Name)
+function Remove-Old($Dir, $Name)
 {
-    Get-ChildItem -Force -File -Path "$BinariesDir\$Name" -ErrorAction SilentlyContinue | Sort-Object CreationTime -Descending | Remove-Item -ErrorAction SilentlyContinue
+    Get-ChildItem -Force -File -Path $Dir\$Name -ErrorAction SilentlyContinue | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Remove-Item -ErrorAction SilentlyContinue
 }
 
 Write-Host "Cleanup Encoder dlls"
 
 $BinariesDir="$PluginDir\Binaries\Win64"
 
-Write-Host "$BinariesDir\Binaries\Win64\*UE4Editor*.dll"
-
-Remove-Old("UE4Editor*.dll")
-Remove-Old("UE4Editor*.pdb")
+Remove-Old -Dir $BinariesDir -Name "UE4Editor*.dll"
+Remove-Old -Dir $BinariesDir -Name "UE4Editor*.pdb"
 
