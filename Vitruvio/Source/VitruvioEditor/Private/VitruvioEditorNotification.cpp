d06bd1f474c4cfe064d447530cc990a92fafa518
Benjamin Neukom <bneukom@esri.com>
2020-07-07 22:49:50 +0200

    Report download and install progress notification (#42)

--- Vitruvio/Source/VitruvioEditor/Private/VitruvioEditorNotification.cpp
+++ Vitruvio/Source/VitruvioEditor/Private/VitruvioEditorNotification.cpp
@@ -1,5 +1,6 @@
 #include "CoreMinimal.h"
 #include "ShaderCompiler.h"
+#include "VitruvioModule.h"
 #include "Widgets/Notifications/GlobalNotification.h"
 #include "Widgets/Notifications/SNotificationList.h"
 
@@ -20,10 +21,34 @@ FVitruvioEditorNotification GShaderCompilingNotification;
 
 bool FVitruvioEditorNotification::ShouldShowNotification(const bool bIsNotificationAlreadyActive) const
 {
-	return false;
+	IModuleInterface* Module = FModuleManager::Get().LoadModule("Vitruvio");
+	VitruvioModule* Vitruvio = dynamic_cast<VitruvioModule*>(Module);
+	return Vitruvio && Vitruvio->GetState() == EPrtState::Downloading || Vitruvio->GetState() == EPrtState::Installing;
 }
 
 void FVitruvioEditorNotification::SetNotificationText(const TSharedPtr<SNotificationItem>& InNotificationItem) const
 {
-	InNotificationItem->SetText(FText::FromString("Vitruvio Notification"));
+	IModuleInterface* Module = FModuleManager::Get().LoadModule("Vitruvio");
+	VitruvioModule* Vitruvio = dynamic_cast<VitruvioModule*>(Module);
+	if (Vitruvio)
+	{
+		EPrtState State = Vitruvio->GetState();
+		if (State == EPrtState::Downloading)
+		{
+			if (Vitruvio->GetDownloadProgress())
+			{
+				const int32 DownloadProgress = static_cast<int32>(Vitruvio->GetDownloadProgress().GetValue() * 100);
+				InNotificationItem->SetText(FText::FromString(FString::Printf(TEXT("Downloading PRT %d%%"), DownloadProgress)));
+			}
+			else
+			{
+				InNotificationItem->SetText(FText::FromString("Downloading PRT"));
+			}
+		}
+		else if (State == EPrtState::Installing)
+		{
+			const int32 InstallProgress = static_cast<int32>(Vitruvio->GetInstallProgress() * 100);
+			InNotificationItem->SetText(FText::FromString(FString::Printf(TEXT("Installing PRT %d%%"), InstallProgress)));
+		}
+	}
 }
