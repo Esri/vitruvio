28dea45cc4e01759b904730a61a9e437be08769d
Benjamin Neukom <bneukom@esri.com>
2020-07-10 19:30:31 +0200

    Add monolithic game module definition (#42)
    This got forgotten when creating a custom define for the game module (because we don't want to override new/delete operators). If we don't add a special define for a monolithic build the packaged (which are monolithic) builds will not work

--- Vitruvio/Source/UnrealGeometryEncoder/Private/UnrealGeometryEncoderModule.cpp
+++ Vitruvio/Source/UnrealGeometryEncoder/Private/UnrealGeometryEncoderModule.cpp
@@ -10,6 +10,18 @@ void UnrealGeometryEncoderModule::ShutdownModule() {}
 
 #undef LOCTEXT_NAMESPACE
 
+#if IS_MONOLITHIC
+
+// If we're linking monolithically we assume all modules are linked in with the main binary.
+#define IMPLEMENT_MODULE_UNREAL(ModuleImplClass, ModuleName)                                                                                         \
+	/** Global registrant object for this module when linked statically */                                                                           \
+	static FStaticallyLinkedModuleRegistrant<ModuleImplClass> ModuleRegistrant##ModuleName(TEXT(#ModuleName));                                       \
+	/* Forced reference to this function is added by the linker to check that each module uses IMPLEMENT_MODULE */                                   \
+	extern "C" void IMPLEMENT_MODULE_##ModuleName() {}                                                                                               \
+	PER_MODULE_BOILERPLATE_ANYLINK(ModuleImplClass, ModuleName)
+
+#else
+
 // We don't want to overwrite new/delete for the module but setting FORCE_ANSI_ALLOCATOR to 1 does not work so we change
 // macros defined in ModuleBoilerplate.h to just not overwrite them.
 
@@ -24,5 +36,7 @@ void UnrealGeometryEncoderModule::ShutdownModule() {}
 	/* Forced reference to this function is added by the linker to check that each module uses IMPLEMENT_MODULE */                                   \
 	extern "C" void IMPLEMENT_MODULE_##ModuleName() {}                                                                                               \
 	PER_MODULE_BOILERPLATE_ANYLINK(ModuleImplClass, ModuleName)
+
+#endif // IS_MONOLITHIC
 
 IMPLEMENT_MODULE_UNREAL(UnrealGeometryEncoderModule, UnrealGeometryEncoder)
