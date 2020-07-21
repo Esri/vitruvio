3ba609a801803c25b75ded2e7b4e28d8df58c7f6
Benjamin Neukom <bneukom@esri.com>
2020-07-21 17:54:30 +0200

    Add encoder program post build step (admin)
    Copies the file into thirdparty folder after a successful build of the encoder library (similar to DatasmithSDK build)

--- Vitruvio/Extras/UnrealGeometryEncoder/UnrealGeometryEncoder.Target.cs
+++ Vitruvio/Extras/UnrealGeometryEncoder/UnrealGeometryEncoder.Target.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
 using UnrealBuildTool;
 using System.Collections.Generic;
+using System.IO;
+using Tools.DotNETCommon;
 
 public class UnrealGeometryEncoderTarget : TargetRules
 {
@@ -29,5 +32,59 @@ public class UnrealGeometryEncoderTarget : TargetRules
 		bUsePDBFiles = true;
 		bHasExports = true;
 		bIsBuildingConsoleApplication = true;
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			AddWindowsPreAndPostBuildSteps(Target);
+		}
+	}
+
+	void AddWindowsPreAndPostBuildSteps(TargetInfo Target)
+	{
+		string VitruvioPath = string.Empty;
+			
+		// Check if Vitruvio Plugin is installed
+		foreach (FileReference Plugin in UnrealBuildTool.Plugins.EnumeratePlugins(Target.ProjectFile))
+		{
+			if (Plugin.FullName.Contains("Vitruvio"))
+			{
+				VitruvioPath = Path.GetDirectoryName(Plugin.FullName);
+			}
+		}
+
+		string ProjectPath = Path.GetDirectoryName(Target.ProjectFile.FullName);
+
+		if (ProjectPath == null)
+		{
+			throw new InvalidOperationException("Project Path is null");
+		}
+		
+		string BinaryFolder = Path.Combine(ProjectPath, "Binaries", "Win64", "UnrealGeometryEncoder");
+		string SourceIncludeFolder = Path.Combine(ProjectPath, "Source", "UnrealGeometryEncoder", "Public");
+		
+		string AllBinaryFolderFiles = Path.Combine(BinaryFolder, "*.*");
+		
+		// We want to delete all old encoder libraries because there might be old builds with different build settings
+		PreBuildSteps.Add(string.Format("echo deleting old encoder libraries \"{0}\"", AllBinaryFolderFiles));
+		PreBuildSteps.Add(string.Format("del /f /q \"{0}\"", AllBinaryFolderFiles));
+		
+		// If Vitruvio is installed, copy the include and library files into the ThirdParty folder of Vitruvio
+		if (!string.IsNullOrEmpty(VitruvioPath))
+		{
+			string VitruvioEncoderLib = Path.Combine(VitruvioPath, "Source", "ThirdParty", "UnrealGeometryEncoderLib");
+			string LibFolder = Path.Combine(VitruvioEncoderLib, "lib", "Win64", "Release");
+
+			string SrcPath = AllBinaryFolderFiles;
+			string DestPath = LibFolder;
+			
+			PostBuildSteps.Add(string.Format("echo Copying \"{0}\" to \"{1}\"", SrcPath, DestPath));
+			PostBuildSteps.Add(string.Format("xcopy \"{0}\" \"{1}\" /R /Y /S", SrcPath, DestPath));
+
+			SrcPath = Path.Combine(SourceIncludeFolder, "*.*");
+			DestPath = Path.Combine(VitruvioEncoderLib, "include");
+			
+			PostBuildSteps.Add(string.Format("echo Copying \"{0}\" to \"{1}\"", SrcPath, DestPath));
+			PostBuildSteps.Add(string.Format("xcopy \"{0}\" \"{1}\" /R /Y /S", SrcPath, DestPath));
+		}
 	}
 }
