c993fdcbcab5c8363f65407a8bf4a013efc97451
Benjamin Neukom <bneukom@esri.com>
2022-05-06 11:07:27 +0200

    Cleanup: Get rid of macro

--- VitruvioHost/Plugins/Vitruvio/Source/VitruvioEditor/Private/VitruvioStyle.cpp
+++ VitruvioHost/Plugins/Vitruvio/Source/VitruvioEditor/Private/VitruvioStyle.cpp
@@ -1,10 +1,8 @@
 #include "VitruvioStyle.h"
 
-#include "Styling/SlateStyleRegistry.h"
 #include "Interfaces/IPluginManager.h"
 #include "SlateOptMacros.h"
-
-#define IMAGE_BRUSH_SVG( RelativePath, ... ) FSlateVectorImageBrush( Style->RootToContentDir(RelativePath, TEXT(".svg")), __VA_ARGS__)
+#include "Styling/SlateStyleRegistry.h"
 
 TSharedPtr<FSlateStyleSet> FVitruvioStyle::StyleSet = nullptr;
 TSharedPtr<ISlateStyle> FVitruvioStyle::Get()
@@ -22,9 +20,6 @@ BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 
 void FVitruvioStyle::Initialize()
 {
-	const FVector2D Icon64x64(64, 64);
-	const FVector2D Icon16x16(16, 16);
-	
 	if (StyleSet.IsValid())
 	{
 		return;
@@ -36,10 +31,15 @@ void FVitruvioStyle::Initialize()
 	StyleSet->SetCoreContentRoot(FPaths::EngineContentDir() / TEXT("Slate"));
 
 	FSlateStyleSet* Style = StyleSet.Get();
-	StyleSet->Set("ClassIcon.VitruvioActor", new IMAGE_BRUSH_SVG("Resources/Vitruvio", Icon16x16));
-	StyleSet->Set("ClassThumbnail.VitruvioActor", new IMAGE_BRUSH_SVG("Resources/Vitruvio", Icon64x64));
-	StyleSet->Set("ClassIcon.VitruvioComponent", new IMAGE_BRUSH_SVG("Resources/Vitruvio", Icon16x16));
-	StyleSet->Set("ClassThumbnail.VitruvioComponent", new IMAGE_BRUSH_SVG("Resources/Vitruvio", Icon64x64));
+
+	const FVector2D Icon64x64(64, 64);
+	const FVector2D Icon16x16(16, 16);
+	const FString ImagePath = Style->RootToContentDir(TEXT("Resources/Vitruvio"), TEXT(".svg"));
+
+	StyleSet->Set("ClassIcon.VitruvioActor", new FSlateVectorImageBrush(ImagePath, Icon16x16));
+	StyleSet->Set("ClassThumbnail.VitruvioActor", new FSlateVectorImageBrush(ImagePath, Icon64x64));
+	StyleSet->Set("ClassIcon.VitruvioComponent", new FSlateVectorImageBrush(ImagePath, Icon16x16));
+	StyleSet->Set("ClassThumbnail.VitruvioComponent", new FSlateVectorImageBrush(ImagePath, Icon64x64));
 
 	FSlateStyleRegistry::RegisterSlateStyle(*StyleSet.Get());
 };
